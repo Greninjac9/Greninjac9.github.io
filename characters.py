8cbfb744d69df98584b54369561c9f9cd8a4b45e
Greninjac9 <leonotsuperhero@gmail.com>
2024-06-05 09:26:22 +0200

    Update characters.py

--- characters.py
+++ characters.py
@@ -2,259 +2,259 @@ Characters = [
     {
         "Nombre": "ARION SHERWIND (ARIONS)", "Curso": "1º",
         "Elemento": "Aire", "Posición": "MD",
-        "Género": "M", "Invocador": "Y",
+        "Género": "M", "Invocador": "EG_Y",
         "EQUIPO": "LOS ARIONS"
     },
     {
         "Nombre": "ARION SHERWIND (CS)", "Curso": "1º",
         "Elemento": "Aire", "Posición": "MD",
-        "Género": "M", "Invocador": "Y",
+        "Género": "M", "Invocador": "EG_Y",
         "EQUIPO": "CHRONO STORM"
     },
     {
         "Nombre": "AXEL BLAZE", "Curso": "2º",
         "Elemento": "Fuego", "Posición": "DL",
-        "Género": "M", "Invocador": "N",
+        "Género": "M", "Invocador": "EG_N",
         "EQUIPO": "RAIMON"
     },
     {
         "Nombre": "BAI LONG", "Curso": "1º",
         "Elemento": "Aire", "Posición": "DL",
-        "Género": "M", "Invocador": "Y",
+        "Género": "M", "Invocador": "EG_Y",
         "EQUIPO": "EQUIPO ZERO"
     },
     {
         "Nombre": "BAI LONG (CS)", "Curso": "1º",
         "Elemento": "Aire", "Posición": "DL",
-        "Género": "M", "Invocador": "Y",
+        "Género": "M", "Invocador": "EG_Y",
         "EQUIPO": "CHRONO STORM"
     },
     {
         "Nombre": "BETA", "Curso": "Por_Definir",
         "Elemento": "Aire", "Posición": "DL",
-        "Género": "F", "Invocador": "Y",
+        "Género": "F", "Invocador": "EG_Y",
         "EQUIPO": "PROTOCOLO OMEGA"
     },
     {
         "Nombre": "BYRON LOVE", "Curso": "2º",
         "Elemento": "Bosque", "Posición": "DL",
-        "Género": "M", "Invocador": "N",
+        "Género": "M", "Invocador": "EG_N",
         "EQUIPO": "ZEUS"
     },
     {
         "Nombre": "DARREN LACHANCE", "Curso": "1º",
         "Elemento": "Bosque", "Posición": "PR",
-        "Género": "M", "Invocador": "N",
+        "Género": "M", "Invocador": "EG_N",
         "EQUIPO": "RAIMON II"
     },
     {
         "Nombre": "DAVE QUAGMIRE", "Curso": "3º",
         "Elemento": "Bosque", "Posición": "DL",
-        "Género": "M", "Invocador": "N",
+        "Género": "M", "Invocador": "EG_N",
         "EQUIPO": "NEO JAPON"
     },
     {
         "Nombre": "DVALIN", "Curso": "3º",
         "Elemento": "Bosque", "Posición": "PR",
-        "Género": "M", "Invocador": "N",
+        "Género": "M", "Invocador": "EG_N",
         "EQUIPO": "ACADEMIA ALIUS"
     },
     {
         "Nombre": "ERIK EAGLE", "Curso": "2º",
         "Elemento": "Bosque", "Posición": "MD",
-        "Género": "M", "Invocador": "N",
+        "Género": "M", "Invocador": "EG_N",
         "EQUIPO": "RAIMON"
     },
     {
         "Nombre": "FEI RUNE", "Curso": "Por_Definir",
         "Elemento": "Bosque", "Posición": "DL",
-        "Género": "M", "Invocador": "N",
+        "Género": "M", "Invocador": "EG_N",
         "EQUIPO": "LOS ARIONS"
     },
     {
         "Nombre": "FEI RUNE (CS)", "Curso": "Por_Definir",
         "Elemento": "Bosque", "Posición": "DL",
-        "Género": "M", "Invocador": "Y",
+        "Género": "M", "Invocador": "EG_Y",
         "EQUIPO": "CHRONO STORM"
     },
     {
         "Nombre": "FLORA", "Curso": "Por_Definir",
         "Elemento": "Aire", "Posición": "DL",
-        "Género": "F", "Invocador": "Y",
+        "Género": "F", "Invocador": "EG_Y",
         "EQUIPO": "DESESPERDIDOS"
     },
     {
         "Nombre": "HECTOR HELIO", "Curso": "2º",
         "Elemento": "Fuego", "Posición": "PR",
-        "Género": "M", "Invocador": "N",
+        "Género": "M", "Invocador": "EG_N",
         "EQUIPO": "THE LITTLE GIANTS"
     },
     {
         "Nombre": "JACK WALLSIDE", "Curso": "1º",
         "Elemento": "Montaña", "Posición": "DF",
-        "Género": "M", "Invocador": "N",
+        "Género": "M", "Invocador": "EG_N",
         "EQUIPO": "RAIMON"
     },
     {
         "Nombre": "JANUS", "Curso": "2º",
         "Elemento": "Bosque", "Posición": "MD",
-        "Género": "M", "Invocador": "N", 
+        "Género": "M", "Invocador": "EG_N", 
         "EQUIPO": "ACADEMIA ALIUS"
     },
     {
         "Nombre": "JUDE SHARP (R.A.)", "Curso": "2º",
         "Elemento": "Bosque", "Posición": "MD",
-        "Género": "M", "Invocador": "N",
+        "Género": "M", "Invocador": "EG_N",
         "EQUIPO": "ROYAL ACADEMY"
     },
     {
         "Nombre": "JOE KING", "Curso": "2º",
         "Elemento": "Fuego", "Posición": "PR",
-        "Género": "M", "Invocador": "N",
+        "Género": "M", "Invocador": "EG_N",
         "EQUIPO": "ROYAL ACADEMY"
     },
     {
         "Nombre": "JORDAN GREENWAY", "Curso": "2º",
         "Elemento": "Bosque", "Posición": "MD",
-        "Género": "M", "Invocador": "N",
+        "Género": "M", "Invocador": "EG_N",
         "EQUIPO": "INAZUMA JAPON"
     },
     {
         "Nombre": "KEVIN DRAGONFLY", "Curso": "2º",
         "Elemento": "Bosque", "Posición": "DL",
-        "Género": "M", "Invocador": "N",
+        "Género": "M", "Invocador": "EG_N",
         "EQUIPO": "RAIMON"
     },
     {
         "Nombre": "MARK EVANS", "Curso": "2º",
         "Elemento": "Montaña", "Posición": "PR",
-        "Género": "M", "Invocador": "N",
+        "Género": "M", "Invocador": "EG_N",
         "EQUIPO": "RAIMON"
     },
     {
         "Nombre": "MARK EVANS (ARIONS)", "Curso": "2º",
         "Elemento": "Montaña", "Posición": "PR",
-        "Género": "M", "Invocador": "Y",
+        "Género": "M", "Invocador": "EG_Y",
         "EQUIPO": "LOS ARIONS"
     },
     {
         "Nombre": "MAX", "Curso": "2º",
         "Elemento": "Aire", "Posición": "DL",
-        "Género": "M", "Invocador": "N",
+        "Género": "M", "Invocador": "EG_N",
         "EQUIPO": "RAIMON"
     },
     {
         "Nombre": "NATHAN SWIFT", "Curso": "2º",
         "Elemento": "Aire", "Posición": "DF",
-        "Género": "M", "Invocador": "N",
+        "Género": "M", "Invocador": "EG_N",
         "EQUIPO": "RAIMON"
     },
     {
         "Nombre": "NELLY RAIMON", "Curso": "2º",
         "Elemento": "Fuego", "Posición": "DL",
-        "Género": "F", "Invocador": "N",
+        "Género": "F", "Invocador": "EG_N",
         "EQUIPO": "EQUIPO FEMENINO"
     },
     {
         "Nombre": "PAOLO BIANCHI", "Curso": "2º",
         "Elemento": "Bosque", "Posición": "DL",
-        "Género": "M", "Invocador": "N",
+        "Género": "M", "Invocador": "EG_N",
         "EQUIPO": "ORFEO"
     },
     {
         "Nombre": "RICCARDO DI RIGO (CS)", "Curso": "2º",
         "Elemento": "Bosque", "Posición": "MD",
-        "Género": "M", "Invocador": "Y",
+        "Género": "M", "Invocador": "EG_Y",
         "EQUIPO": "CHRONO STORM"
     },
     {
         "Nombre": "SAM KINCAID", "Curso": "1º",
         "Elemento": "Fuego", "Posición": "DF",
-        "Género": "M", "Invocador": "N",
+        "Género": "M", "Invocador": "EG_N",
         "EQUIPO": "RAIMON"
     },
     {
         "Nombre": "SEYMOUR HILLMAN", "Curso": "ADULTO",
         "Elemento": "Montaña", "Posición": "DT",
-        "Género": "M", "Invocador": "N",
+        "Género": "M", "Invocador": "EG_N",
         "EQUIPO": "RAIMON"
     },
     {
         "Nombre": "SHAWN FROSTE", "Curso": "2º",
         "Elemento": "Aire", "Posición": "DL",
-        "Género": "M", "Invocador": "N",
+        "Género": "M", "Invocador": "EG_N",
         "EQUIPO": "INAZUMA JAPON"
     },
     {
         "Nombre": "SHAWN FROSTE (AIDEN)", "Curso": "2º",
         "Elemento": "Aire", "Posición": "DL",
-        "Género": "M", "Invocador": "N",
+        "Género": "M", "Invocador": "EG_N",
         "EQUIPO": "RAIMON II"
     },
     {
         "Nombre": "SHAWN FROSTE (DF)", "Curso": "2º",
         "Elemento": "Aire", "Posición": "DF",
-        "Género": "M", "Invocador": "N",
+        "Género": "M", "Invocador": "EG_N",
         "EQUIPO": "RAIMON II"
     },
     {
         "Nombre": "TEZCAT", "Curso": "1º",
         "Elemento": "Bosque", "Posición": "DL",
-        "Género": "M", "Invocador": "Y",
+        "Género": "M", "Invocador": "EG_Y",
         "EQUIPO": "EQUIPO ZERO"
     },
     {
         "Nombre": "TOD IRONSIDE", "Curso": "1º",
         "Elemento": "Fuego", "Posición": "DF",
-        "Género": "M", "Invocador": "N",
+        "Género": "M", "Invocador": "EG_N",
         "EQUIPO": "RAIMON"
     },
     {
         "Nombre": "TORI", "Curso": "2º",
         "Elemento": "Aire", "Posición": "MD",
-        "Género": "F", "Invocador": "N",
+        "Género": "F", "Invocador": "EG_N",
         "EQUIPO": "RAIMON II"
     },
     {
         "Nombre": "TYLER MURDOCK", "Curso": "2º",
         "Elemento": "Fuego", "Posición": "DL",
-        "Género": "M", "Invocador": "N",
+        "Género": "M", "Invocador": "EG_N",
         "EQUIPO": "NEO JAPON"
     },
     {
         "Nombre": "VICTOR BLADE (CS)", "Curso": "1º",
         "Elemento": "Fuego", "Posición": "DL",
-        "Género": "M", "Invocador": "Y",
+        "Género": "M", "Invocador": "EG_Y",
         "EQUIPO": "CHRONO STORM"
     },
     {
         "Nombre": "VLADIMIR BLADE", "Curso": "ADULTO",
         "Elemento": "Fuego", "Posición": "DL",
-        "Género": "M", "Invocador": "Y",
+        "Género": "M", "Invocador": "EG_Y",
         "EQUIPO": "LOS ARIONS"
     },
     {
         "Nombre": "WILLIAM GLASS", "Curso": "2º",
         "Elemento": "Bosque", "Posición": "DL",
-        "Género": "M", "Invocador": "N",
+        "Género": "M", "Invocador": "EG_N",
         "EQUIPO": "RAIMON"
     },
     {
         "Nombre": "XAVIER FOSTER", "Curso": "2º",
         "Elemento": "Fuego", "Posición": "DL",
-        "Género": "M", "Invocador": "N",
+        "Género": "M", "Invocador": "EG_N",
         "EQUIPO": "INAZUMA JAPON"
     },
     {
         "Nombre": "XENE", "Curso": "2º",
         "Elemento": "Fuego", "Posición": "DL",
-        "Género": "M", "Invocador": "N",
+        "Género": "M", "Invocador": "EG_N",
         "EQUIPO": "ACADEMIA ALIUS"
     },
     {
         "Nombre": "ZANARK AVALONIC (CS)", "Curso": "Por_Definir",
         "Elemento": "Montaña", "Posición": "DL",
-        "Género": "M", "Invocador": "Y",
+        "Género": "M", "Invocador": "EG_Y",
         "EQUIPO": "CHRONO STORM"
     }
 ]
