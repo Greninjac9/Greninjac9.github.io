3b8d11b154a70a48a6e5c647eeda94d36a7b74eb
Greninjac9 <leonotsuperhero@gmail.com>
2024-06-08 13:37:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -33,9 +33,9 @@ def CheckValues():
             Split_Key = Characters[g_index][key].split()
             Split_char = character[key].split()
             if N == 1 and Split_Key[0] == Split_char[0] and Split_Key[0] == Split_char[0]:
-                color = "FFFF00"
+                color = "#FFFF00"
             elif N == 7 and Split_key[0] == "RAIMON" and Split_char[0] == "RAIMON":
-                color = "FFFF00"
+                color = "#FFFF00"
         if Characters[g_index][key] == character[key]:
             color = "green"
         if key in ["Elemento", "Género","Invocador"]:
